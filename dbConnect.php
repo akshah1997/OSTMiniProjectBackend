b5f6bb5fc840b4ede8f32741ebad6f3cb896dfd8
Akshay Shah <akshah1997@gmail.com>
2017-09-20 22:39:12 +0530

    Formatting and indentation
    Closed the php files. Done proper spacing and indentation.

--- dbConnect.php
+++ dbConnect.php
@@ -3,5 +3,6 @@
  define('USER','id2942109_root');
  define('PASS','Ostproject');
  define('DB','id2942109_userdetails');
- 
+
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+ ?>
